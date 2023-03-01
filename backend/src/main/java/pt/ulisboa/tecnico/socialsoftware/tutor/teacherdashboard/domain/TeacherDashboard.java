8e08da4a71e5a49117e967ad4fe65abe5e7cd915
Diogo Soares <diogo.m.g.soares@tecnico.ulisboa.pt>
2023-03-01 23:45:45 +0000

    feat: Implemented QuestionStats related functions in TeacherDashboard closes #23

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/domain/TeacherDashboard.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/domain/TeacherDashboard.java
@@ -4,8 +4,10 @@ import pt.ulisboa.tecnico.socialsoftware.tutor.execution.domain.CourseExecution;
 import pt.ulisboa.tecnico.socialsoftware.tutor.impexp.domain.DomainEntity;
 import pt.ulisboa.tecnico.socialsoftware.tutor.impexp.domain.Visitor;
 import pt.ulisboa.tecnico.socialsoftware.tutor.user.domain.Teacher;
+import pt.ulisboa.tecnico.socialsoftware.tutor.teacherdashboard.domain.QuestionStats;
 
 import javax.persistence.*;
+import java.util.*;
 
 
 @Entity
@@ -21,6 +23,9 @@ public class TeacherDashboard implements DomainEntity {
     @ManyToOne
     private Teacher teacher;
 
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "teacherDashboard", orphanRemoval = true)
+    private List<QuestionStats> questionStats = new ArrayList<QuestionStats>(); 
+
     public TeacherDashboard() {
     }
 
@@ -28,7 +33,7 @@ public class TeacherDashboard implements DomainEntity {
         setCourseExecution(courseExecution);
         setTeacher(teacher);
     }
-
+    
     public void remove() {
         teacher.getDashboards().remove(this);
         teacher = null;
@@ -55,8 +60,21 @@ public class TeacherDashboard implements DomainEntity {
         this.teacher.addDashboard(this);
     }
 
+    public List<QuestionStats> getQuestionStats() {
+        return questionStats;
+    }
+
+    public void addQuestionStats(QuestionStats questionStats) {
+        this.questionStats.add(questionStats);
+    }
+
     public void accept(Visitor visitor) {
         // Only used for XML generation
+    }
+
+    public void update(){ 
+        for (QuestionStats q: this.getQuestionStats())
+            q.update();          
     }
 
     @Override
